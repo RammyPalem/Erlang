cf41b3a248b1fff9d9989746a0cf4388a6453d0b
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-10-30 13:43:27 -0500

    Update erlang.pp

--- erlang.pp
+++ erlang.pp
@@ -5,6 +5,7 @@ class erlang_install {
   exec { 'check-erlang-version':
     command => "/usr/lib/erlang/bin/erl -eval 'io:format(\"~s\", [erlang:system_info(otp_release)]), halt().' -noshell | grep -q $desired_version",
     path    => ['/usr/lib/erlang/bin', '/bin', '/usr/bin'],
+    onlyif  => "test -x /usr/lib/erlang/bin/erl",
   }
 
   # Install Clang-related packages
@@ -20,8 +21,7 @@ class erlang_install {
   exec { 'install-erlang':
     command  => "wget https://www.erlang.org/download/otp_src_${desired_version}.tar.gz -O /tmp/otp_src_${desired_version}.tar.gz && tar -xzvf /tmp/otp_src_${desired_version}.tar.gz -C /tmp && cd /tmp/otp_src_${desired_version} && ./configure && make && make install",
     creates  => "/usr/local/lib/erlang/erts-${desired_version}",
-    unless   => "/usr/lib/erlang/bin/erl -eval 'io:format(\"~s\", [erlang:system_info(otp_release)]), halt().' -noshell | grep -q $desired_version",
+    unless   => "test -x /usr/lib/erlang/bin/erl && /usr/lib/erlang/bin/erl -eval 'io:format(\"~s\", [erlang:system_info(otp_release)]), halt().' -noshell | grep -q $desired_version",
     require  => [Exec['check-erlang-version'], Package['clang-format', 'clang-tools', 'clang']],
   }
 }
-
