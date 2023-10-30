3795d05a2eb3745ba58490e1c21c9c21239f21dd
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-10-30 12:16:01 -0500

    Update erlang_v4.pp

--- erlang_v4.pp
+++ erlang_v4.pp
@@ -1,6 +1,20 @@
 class erlang_install {
   $desired_version = '20.3'
 
+# Add the Erlang Solutions repository
+  apt::source { 'erlang-solutions':
+    location   => 'https://packages.erlang-solutions.com/ubuntu',
+    key        => {
+      id     => '9F77F57AEF05CA5D',
+      source => 'https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc',
+    },
+    release    => 'focal', # Use 'focal' for Ubuntu 20.04
+    repos      => 'contrib',
+    include    => {
+      src  => false,
+      deb  => true,
+    },
+  }
   # Check if Erlang is installed
   $erlang_installed = package { 'esl-erlang':
     ensure => 'installed',
