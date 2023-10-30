745076eecd3b4937e0ff99005ebb1d67600838cc
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-10-30 14:57:00 -0500

    Update erlang.pp

--- erlang.pp
+++ erlang.pp
@@ -1,6 +1,21 @@
 class erlang_install {
   $desired_version = '20.3'
 
+  # Add the Erlang Solutions repository
+  apt::source { 'erlang-solutions':
+    location   => 'https://packages.erlang-solutions.com/ubuntu',
+    key        => {
+      id     => '9F77F57AEF05CA5D',
+      source => 'https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc',
+    },
+    release    => 'focal', # Use 'focal' for Ubuntu 20.04
+    repos      => 'contrib',
+    include    => {
+      src  => false,
+      deb  => true,
+    },
+  }
+
   # Check if Erlang is already installed at the desired version
   exec { 'check-erlang-version':
     command => "/usr/lib/erlang/bin/erl -eval 'io:format(\"~s\", [erlang:system_info(otp_release)]), halt().' -noshell | grep -q $desired_version",
@@ -18,11 +33,9 @@ class erlang_install {
     ensure => 'installed',
   }
 
-  # Download and install Erlang/OTP 20.3 if not found or not at the desired version
-  exec { 'install-erlang':
-    command  => "wget https://www.erlang.org/download/otp_src_${desired_version}.tar.gz -O /tmp/otp_src_${desired_version}.tar.gz && tar -xzvf /tmp/otp_src_${desired_version}.tar.gz -C /tmp && cd /tmp/otp_src_${desired_version} && ./configure && make && make install",
-    creates  => "/usr/local/lib/erlang/erts-${desired_version}",
-    unless   => "/usr/bin/test -x /usr/lib/erlang/bin/erl && /usr/lib/erlang/bin/erl -eval 'io:format(\"~s\", [erlang:system_info(otp_release)]), halt().' -noshell | grep -q $desired_version",
-    require  => [Exec['check-erlang-version'], Package['clang-format', 'clang-tools', 'clang', 'llvm-runtime']],
+  # Install Erlang/OTP from the repository
+  package { 'esl-erlang':
+    ensure => $desired_version,
+    require => [Exec['check-erlang-version'], Package['clang-format', 'clang-tools', 'clang', 'llvm-runtime']],
   }
 }
