c548277651d7b7a998ff835d3d83cbcba8ce37ed
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-01 16:35:31 -0500

    Update erlanf-fianl.sh

--- erlanf-fianl.sh
+++ erlanf-fianl.sh
@@ -6,19 +6,16 @@ installed_version=$(erl -eval 'erlang:display(erlang:system_info(otp_release)), 
 if [ -z "$installed_version" ]; then
   echo "Erlang/OTP is not installed."
   # Proceed with the installation of Erlang/OTP version 20.0
-  erl_version="20.0"
+  erl_version="20"
 else
   echo "Installed Erlang/OTP version: $installed_version"
-  if [ "${installed_version%%.*}" -eq 20 ]; then
+  if [ "$installed_version" = "20" ]; then
     echo "Erlang/OTP version 20 is already installed. No need to install."
     exit 0
-  elif [ "$installed_version" != "20.0" ]; then
-    echo "Erlang/OTP version $installed_version is installed, but we need version 20.0."
-    echo "Proceeding with the installation of Erlang/OTP version 20.0..."
-    erl_version="20.0"
   else
-    echo "Erlang/OTP version 20.0 is already installed. No need to install."
-    exit 0
+    echo "Erlang/OTP version $installed_version is installed, but we need version 20."
+    echo "Proceeding with the installation of Erlang/OTP version 20..."
+    erl_version="20"
   fi
 fi
 
