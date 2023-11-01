c2fac6d436f16c2718348059772a8a61d918c455
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-01 16:47:49 -0500

    Update erlanf-fianl.sh

--- erlanf-fianl.sh
+++ erlanf-fianl.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# Check the installed version using the erl command
-installed_version=$(erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell 2>/dev/null)
-
-if [ -z "$installed_version" ]; then
-  echo "Erlang/OTP is not installed."
-  # Proceed with the installation of Erlang/OTP version 20.0
-  erl_version="20"
-else
-  echo "Installed Erlang/OTP version: $installed_version"
-  if [ "$installed_version" = "20" ]; then
-    echo "Erlang/OTP version 20 is already installed. No need to install."
-    exit 0
-  else
-    echo "Erlang/OTP version $installed_version is installed, but we need version 20."
-    echo "Proceeding with the installation of Erlang/OTP version 20..."
-    erl_version="20"
-  fi
-fi
+# Proceed with the installation of Erlang/OTP version 20.0
+erl_version="20.0"
 
 # Rest of the installation script (install Erlang/OTP version $erl_version)
 # ...
+
+# Update the package manager
+sudo apt-get update
+
+# Install required dependencies
+sudo apt-get install -y build-essential autoconf libncurses5-dev libssl-dev m4 java-common clang clang-format
+
+# Download the Erlang/OTP source code (official Erlang site, adjust the version if needed)
+wget --no-check-certificate "https://www.erlang.org/download/otp_src_$erl_version.tar.gz"
+tar xvzf "otp_src_$erl_version.tar.gz"
+
+# Build and install Erlang/OTP
+cd "otp_src_$erl_version"
+./otp_build autoconf
+./configure
+make
+sudo make install
+
+# Clean up
+cd ..
+rm -rf "otp_src_$erl_version.tar.gz" "otp_src_$erl_version"
+
+# Verify Erlang/OTP installation
+erl -version
