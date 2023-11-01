f49ec73f636bbc169e39cce7fa4cc54f053203d9
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-01 15:54:42 -0500

    Update Arlan-install.sh

--- Arlan-install.sh
+++ Arlan-install.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
 
-# Set the desired Erlang/OTP version
-OTP_VERSION="20"
+# Check if Erlang/OTP is installed
+erl_version=$(erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell 2>/dev/null)
 
-# Check if Erlang/OTP version is already installed
-ERL_CHECK=$(erl -eval "erlang:display(erlang:system_info(otp_release)), halt()." -noshell 2> /dev/null)
-if [[ $ERL_CHECK == $OTP_VERSION* ]]; then
-    echo "Erlang/OTP $OTP_VERSION is already installed."
+if [ -z "$erl_version" ]; then
+  echo "Erlang/OTP is not installed."
+  # Proceed with the installation of Erlang/OTP version 20.0
+  erl_version="20.0"
+else
+  # Check if Erlang/OTP version is not 20.0
+  if [ "$erl_version" != "20.0" ]; then
+    echo "Erlang/OTP version $erl_version is installed, but we need version 20.0."
+    echo "Proceeding with the installation of Erlang/OTP version 20.0..."
+    erl_version="20.0"
+  else
+    echo "Erlang/OTP version 20.0 is already installed. No need to install."
     exit 0
+  fi
 fi
 
-# Update package lists
+# Rest of the installation script (install Erlang/OTP version $erl_version)
+# Update the package manager
 sudo apt-get update
 
-# Install dependencies
-sudo apt-get install -y build-essential wget
+# Install required dependencies
+sudo apt-get install -y build-essential autoconf libncurses5-dev libssl-dev m4 java-common clang clang-format
 
-# Download Erlang/OTP source code
-wget "https://github.com/erlang/otp/archive/OTP-${OTP_VERSION}.tar.gz" -O otp-${OTP_VERSION}.tar.gz
-tar -xf otp-${OTP_VERSION}.tar.gz
-rm otp-${OTP_VERSION}.tar.gz
-cd otp-OTP-${OTP_VERSION}
+# Download the Erlang/OTP source code (official Erlang site, adjust the version if needed)
+wget --no-check-certificate "https://www.erlang.org/download/otp_src_$erl_version.tar.gz"
+tar xvzf "otp_src_$erl_version.tar.gz"
 
-# Configure and build Erlang/OTP
+# Build and install Erlang/OTP
+cd "otp_src_$erl_version"
 ./otp_build autoconf
 ./configure
 make
 sudo make install
 
-# Cleanup
+# Clean up
 cd ..
-rm -rf otp-OTP-${OTP_VERSION}
+rm -rf "otp_src_$erl_version.tar.gz" "otp_src_$erl_version"
 
-echo "Erlang/OTP $OTP_VERSION has been successfully installed."
+# Verify Erlang/OTP installation
+erl -version
