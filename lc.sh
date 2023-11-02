ec55e2d5511e855bd25bf1ebb2f01a375f300c51
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-02 12:07:03 -0500

    Update lc.sh

--- lc.sh
+++ lc.sh
@@ -1,4 +1,8 @@
-"sleep 30s",
+"UserData": {
+					"Fn::Base64": {
+						"Fn::Join": [
+							"\n", [
+								"#!/bin/bash",
 								"apt-get update",
 								"apt-get install -y build-essential autoconf libncurses5-dev m4 java-common wget libssl-dev clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang automake",
 								"wget https://www.erlang.org/download/otp_src_20.3.tar.gz",
@@ -9,4 +13,8 @@
 								"sudo make install",
 								"cd ..",
 								"rm -rf OTP-20.3.tar.gz otp-OTP-20.3",
-								
+
+							]
+						]
+					}
+				},
