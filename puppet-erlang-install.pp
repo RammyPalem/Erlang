56a9f8e0b3d18a0ff90e0e1c3b816b7e50e1d804
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-03 16:06:01 -0500

    Update puppet-erlang-install.pp

--- puppet-erlang-install.pp
+++ puppet-erlang-install.pp
@@ -1,19 +1,18 @@
-class erlang_install {
-  exec { 'install_erlang':
-    command     => '/tmp/script.sh',
-    user        => 'root',
-    group       => 'root',
-    timeout     => 1200, # Timeout in seconds (20 minutes)
-    unless      => 'erl -eval "io:format(~p~n, [erlang:system_info(otp_release)])" -s init stop | grep -q "20.3"',
-    path        => '/usr/bin:/bin',
-    require     => File['/tmp/script.sh'], # Ensure the script is present
-    logoutput   => true,
+class erlang_check_and_execute {
+  # Check the installed Erlang version
+  exec { 'check_erlang_version':
+    command   => 'erl -eval "io:format(\"~s~n\", [erlang:system_info(otp_release)])" -s init stop',
+    path      => '/usr/local/bin:/usr/bin', # Adjust the path as needed
+    logoutput => true,
+    unless    => 'echo "$EXEC_OUTPUT" | grep -q "Erlang/OTP 20"',
+    notify    => Exec['execute_script'], # Trigger script execution if version matches
   }
 
-  file { '/tmp/script.sh':
-    source => 'puppet:///modules/your_module/script.sh', # Path to your script
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755', # Make the script executable
+  # Execute the shell script
+  exec { 'execute_script':
+    command   => '/path/to/your_script.sh', # Replace with the actual path to your script
+    path      => '/usr/local/bin:/usr/bin',  # Adjust the path as needed
+    require   => Exec['check_erlang_version'],
+    onlyif    => 'test -f /path/to/your_script.sh', # Ensure the script exists
   }
 }
