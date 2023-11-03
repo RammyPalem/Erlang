66f0c58c3cb315daa4f5195b6b0d65117eac959c
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-03 15:08:09 -0500

    Update install_erlang.sh

--- install_erlang.sh
+++ install_erlang.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Add the Erlang Solutions repository GPG key
-wget -O - https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo apt-key add -
-
-# Add the Erlang Solutions repository to your apt sources
-echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | sudo tee /etc/apt/sources.list.d/erlang-solutions.list
-
-# Update your apt package list
+# Install dependencies for building Erlang
 sudo apt-get update
+sudo apt-get install -y build-essential libncurses5-dev libssl-dev m4
 
-# Install Erlang/OTP 20.3
-sudo apt-get install -y erlang=1:20.3
+# Clone the kerl repository if not already installed
+if [ ! -d ~/.kerl ]; then
+  git clone https://github.com/kerl/kerl.git ~/.kerl
+fi
+
+# Build and install Erlang/OTP 20.3
+~/.kerl/kerl build 20.3 20.3
+~/.kerl/kerl install 20.3 ~/.erlang/20.3
+
+# Activate Erlang/OTP 20.3
+. ~/.kerl/installs/20.3/activate
 
 # Verify the installed Erlang version
-erl -eval "io:format(\"Erlang/OTP ~s~n\", [erlang:system_info(otp_release)]), halt()."
-
-# Remove the repository file
-sudo rm /etc/apt/sources.list.d/erlang-solutions.list
-
-# Update apt once more
-sudo apt-get update
+erl -eval "io:format(\"Erlang/OTP ~s~n\", [erlang:system_info(otp_release)])" -s init stop
