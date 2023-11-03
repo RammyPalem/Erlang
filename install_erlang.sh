5ded4ee0e1c2ce609f495397a91293ea3768c2b9
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-03 16:55:21 -0500

    Update install_erlang.sh

--- install_erlang.sh
+++ install_erlang.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+#!/bin/bash
 
+# Function to compare Erlang version
+check_erlang_version() {
+  installed_version=$(erl -eval "io:format(\"~s~n\", [erlang:system_info(otp_release)])" -s init stop)
+  desired_version="20.3"  # Replace with your desired version
+
+  if [ "$installed_version" = "$desired_version" ]; then
+    echo "Erlang version is already the desired version ($desired_version). Exiting."
+    exit 0
+  else
+    echo "Erlang version is not the desired version. Proceeding with script execution."
+  fi
+}
+
+# Call the function to check Erlang version
+check_erlang_version
+
+# Add your script logic here
+# ...
 # Install dependencies for building Erlang
 sudo apt-get update
 sudo apt-get install -y build-essential libncurses5-dev libssl-dev m4
