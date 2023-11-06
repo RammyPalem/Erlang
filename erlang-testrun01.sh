109eaf11bbe5e1686707cae9d4112e35ef87f01e
RammyPalem <138802915+RammyPalem@users.noreply.github.com>
2023-11-06 11:02:53 -0600

    Update erlang-testrun01.sh

--- erlang-testrun01.sh
+++ erlang-testrun01.sh
@@ -1,6 +1,6 @@
 # Install required dependencies
 sudo apt-get update -y
-sudo apt-get install -y build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-glx libgl1-mesa-dev libssl-dev
+sudo apt-get install wget libssl-dev clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang automake autoconf libncurses5-dev
 
 # Download and extract Erlang/OTP source code
 wget https://github.com/erlang/otp/archive/OTP-20.3.tar.gz
